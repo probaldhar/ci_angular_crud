907dae8fdabfdbcd274bbb53942e19a0979c17b8
Probal Chandra Dhar <probal@schedulegofer.com>
2018-09-09 23:52:28 -0700

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+
+#CODEIGNITER ANGULARJS MySQL CRUD
+
+<b>Resource:</b> 
+1. Angularjs
+2. Codeigniter 3
+3. MySQL
+ 
+
+
+<b>Feature:</b> 
+1. Create 
+2. Update 
+3. View 
+4. Delete
+5. Search Option
+6. Pagination
+
+<a  href="http://dev.codeenable.com/angularjs-ci-mysql-crud-demo/" target="_blank" >Demo</a>
+
---
+++ angularjs/app.js
@@ -0,0 +1,34 @@
+
+var myApp = angular.module('example_codeenable', ['ui.router', 'ui.bootstrap']);
+
+myApp.constant('APP_CONSTANTS', {
+	'BASE_URL': 'http://localhost:8000/ci_angular_crud',
+	
+}) ;
+
+myApp.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+            .state('/', {
+                url: '/',
+                templateUrl: 'templates/student.html',
+                controller: 'student_contrloer',
+                controllerAs: "std_ctrl",
+              
+                resolve: {
+                    'title': ['$rootScope', function ($rootScope) {
+                            $rootScope.title = "ANGULARJS CODEGINITER MySQL CRUD";
+                        }]
+                }
+
+            })
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
+});
+
+
